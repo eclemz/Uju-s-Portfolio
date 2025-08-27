6a7c7a5fb50502ed9afdef15f60c9d3646be5c87
eclemz <bizyjingles@yahoo.com>
2025-08-27 02:59:03 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   // darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./.storybook/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
